3f3394c8525712b6f58164826247486430b4927d
meda2001 <87261349+meda2001@users.noreply.github.com>
2023-03-27 05:57:46 +0000

    Different ways to write Hello world

---
+++ Ways to write Hello world/main.py
@@ -0,0 +1,20 @@
+# different ways to write hello world program
+
+
+message="Hello world"
+print(message)
+
+
+
+
+print("Hello wrold")
+
+
+
+
+message1='Hello'
+message2=" world"
+print(message1+message2)
+
+
+#... still many more
