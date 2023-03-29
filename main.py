7d6e8509fc495377738eb48b7723955a6e9af5ac
meda2001 <87261349+meda2001@users.noreply.github.com>
2023-03-29 05:27:37 +0000

    Numbers and Type casting

---
+++ main.py
@@ -0,0 +1,28 @@
+# let dicuss on Number data types  and type casting
+'''
+remember these words int(),float(),complex()
+
+'''
+# type casting means convert one data type to the another data type
+#################################################################int to float  and float to int
+
+myInt = 5
+myFloat = float(myInt)
+print(myFloat) # to get float value
+_myInt=int(myFloat)
+print(_myInt) # to get integer value
+
+########################################################## float to complex and complex to float
+
+_myFloat=5.6
+myComplex = float(_myFloat)
+print(myComplex)
+myFloat_= float(myComplex)
+print(myFloat_)
+
+
+###########################################################complex to int and int complex
+
+'''
+why cannot you can try this ?
+'''
